b455bf2d33f764d3cc038dec3f56e985a2e509c3
ujjwalchaudhari19-cyber <ujjwalchaudhari19@gmail.com>
2026-07-15 19:11:06 +0530

    Implementing linkedlist

---
+++ Linkedlist.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+typedef struct node{
+  int number;
+  struct node *next;
+}node;
+
+int main(void){
+  int n;
+  printf("Number of nodes: ");
+  scanf("%i", &n);
+}
+
+int insert(node *list, int value){
+
+}
